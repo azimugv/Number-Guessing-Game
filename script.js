65e0c7bd1643b29bbad753962dd188ade9f83a94
Md. Abdullah Al Azim <67109691+azimugv@users.noreply.github.com>
2022-08-26 02:42:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-var msg1 = document.getElementById("message1");
-
 var answer = Math.floor(Math.random()*10) + 1;
 
-
+var a = 0;
 
 function play(){
 
-   for(var i = 0; i<3 ; i++){
+   for(var i = 3; i>0 ; i--){
+    a += 1;
     var user_guess = prompt("Guess between 1 and 10");
     if(user_guess > answer){
-        alert("Correct answer in smaller");
+        alert(`Correct answer in smaller\nYou have ${i-1} chances left`);
     }else if(user_guess < answer){
-        alert("Coreect answer is greater");
+        alert(`Coreect answer is greater\nYou have ${i-1} chances left`);
     }else if( user_guess == answer){
         alert("You Win");
         break;
     }
    }
-   if( i == 3){
+   if( a == 3){
     alert("You lose!");
     document.getElementById('my_btn').disabled = true;
+    document.getElementById('my_btn2').style="display:block";
+    document.getElementById('my_btn2').innerHTML = "Restart";
    }
 }
